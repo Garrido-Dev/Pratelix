d1239b4b920de7b48c7a7cb431a3d32ce2243807
Juan Garrido <juamgarrido96@Gmail.com>
2026-09-23 10:08:53 -0300

    Alterações de Host do Agente para agents/item-update-issue-resolution

--- src/pages/items/UpdateItem.jsx
+++ src/pages/items/UpdateItem.jsx
@@ -1,5 +1,15 @@
-export default function UpdateItem(){
-    return(
-        <h2>Atualizar Item</h2>
-    )
+import { useParams } from "react-router-dom"
+import ItemForm from "../../components/itemForm"
+import useStock from "/src/hooks/useStock.js"
+
+export default function UpdateItem() {
+    const { id } = useParams()
+    const { getItem } = useStock()
+    const itemToUpdate = getItem(id)
+
+    if (!itemToUpdate) {
+        return <h2>Item não encontrado!</h2>
+    }
+
+    return <ItemForm key={itemToUpdate.id} itemToUpdate={itemToUpdate} />
 }
